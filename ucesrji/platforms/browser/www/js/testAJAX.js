1d825c409ac98e45c1a361fb0a18455a46f29ecf
ruiiiiiur <ruiiiiiur@gmail.com>
2019-02-28 11:44:40 -0500

    ajax

---
+++ ucesrji/platforms/browser/www/js/testAJAX.js
@@ -0,0 +1,47 @@
+var xhr; // define the global variable to process the AJAX request
+function callDivChange() {
+//xhr = new XMLHttpRequest();
+//xhr.open("GET", "test.html", true);
+//xhr.onreadystatechange = processDivChange;
+//xhr.send();
+
+xhr = new XMLHttpRequest();
+var filename = document.getElementById("filename").value;
+xhr.open("GET", filename, true);
+xhr.onreadystatechange = processDivChange;
+xhr.send();
+
+}
+function processDivChange() {
+if (xhr.readyState < 4) // while waiting response from server
+document.getElementById('div1').innerHTML = "Loading...";
+else if (xhr.readyState === 4) { // 4 = Response from server has been completely loaded.
+if (xhr.status == 200 && xhr.status < 300)
+// http status between 200 to 299 are all successful
+document.getElementById('div1').innerHTML = xhr.responseText;
+}
+}
+
+var xhrNode;
+function callDivNodeJSChange() {
+xhrNode = new XMLHttpRequest();
+var url = "http://developer.cege.ucl.ac.uk:"+httpPortNumber;
+xhrNode.open("GET", url, true);
+xhrNode.onreadystatechange = processDivNodeJSChange;
+try {
+xhrNode.setRequestHeader("Content-Type", "application/x-www-formurlencoded");
+}
+catch (e) {
+// this only works in internet explorer
+}
+xhrNode.send();
+}
+function processDivNodeJSChange() {
+if (xhrNode.readyState < 4) // while waiting response from server
+{document.getElementById('ajaxtext').innerHTML = "Loading...";}
+else if (xhrNode.readyState === 4) { // 4 = Response from server has been completely loaded.
+if (xhrNode.status == 200 && xhrNode.status < 300)
+// http status between 200 to 299 are all successful
+document.getElementById('ajaxtext').innerHTML = xhrNode.responseText;
+}
+}
