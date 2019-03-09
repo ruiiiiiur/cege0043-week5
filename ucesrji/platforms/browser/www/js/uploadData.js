46d28200299edc05b554cdcaa35ccf0d410fda56
ruiiiiiur <jiangrui1123@outlook.com>
2019-03-09 07:01:28 -0500

    new

--- ucesrji/platforms/browser/www/js/uploadData.js
+++ ucesrji/platforms/browser/www/js/uploadData.js
@@ -39,12 +39,19 @@ processData(postString);
 var client; // the global variable that holds the request
 
 
+
 function processData(postString) {
 client = new XMLHttpRequest();
-client.open('POST','http://developer.cege.ucl.ac.uk:30284/reflectData',true);
+postString = postString + "&port_id=" + httpPortNumber;
+var url = 'http://developer.cege.ucl.ac.uk:'+ httpPortNumber + "/uploadData";
+client.open('POST',url,true);
 client.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 client.onreadystatechange = dataUploaded;
-client.send(postString);}
+client.send(postString);
+}
+
+
+
 
 // create the code to wait for the response from the data server, and process the response once it is received
 function dataUploaded() {
